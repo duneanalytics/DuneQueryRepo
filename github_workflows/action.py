03ef8440aaa3cc61e6559cf1a3c8acc5e73764fa
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-11-28 10:18:23 -0500

    read from secrets?

--- github_workflows/action.py
+++ github_workflows/action.py
@@ -1,1 +1,17 @@
 print('test action hello wrld')
+
+import yaml
+from dune_client.client import DuneClient
+
+dune = DuneClient.from_env() 
+
+# Read the queries.yml file
+with open('queries.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+# Extract the query_ids from the data
+query_ids = [id for id in data['query_ids']]
+
+for id in query_ids:
+    query = dune.get_query(id)
+    print(query)
