44abfb079106471edad34d8a9215684082ad6b01
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-11-28 19:48:00 -0500

    fix

--- queries/query_3237742.sql
+++ queries/query_3237742.sql
@@ -14,15 +14,3 @@ WHERE
 GROUP BY
   1,
   2
--- Weekly DEX Aggregator volume
-SELECT
-  project,
-  DATE_TRUNC('week', block_time),
-  SUM(CAST(amount_usd AS DOUBLE)) AS usd_volume
-FROM
-   dex_aggregator.trades AS t /* AND block_time < date_trunc('week', Now()) -- Add this line to see stats from current week */
-WHERE
- block_time > NOW() - INTERVAL '365' day
-GROUP BY
-  1,
-  2
