41b3a8876b493e4f1f193b0bdd946cb0740bb151
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2023-11-29 16:35:49 -0500

    push

--- scripts/preview_query.py
+++ scripts/preview_query.py
@@ -12,19 +12,28 @@ dune = DuneClient.from_env()
 #get id passed in python script invoke
 id = sys.argv[1]
 
-query_file = os.path.join(os.path.dirname(__file__), '..', 'queries', f'query_{id}.sql')
+queries_path = os.path.join(os.path.dirname(__file__), '..', 'queries')
+files = os.listdir(queries_path)
+found_files = [file for file in files if str(id) == file.split('___')[-1].split('.')[0]]
 
-print('getting 20 line preview for query {}...'.format(id))
+if len(found_files) != 0:
+    query_file = os.path.join(os.path.dirname(__file__), '..', 'queries', found_files[0])
 
-with open(query_file, 'r', encoding='utf-8') as file:
-    query_text = file.read()
+    print('getting 20 line preview for query {}...'.format(id))
 
-results = dune.run_sql(query_text + '\n limit 20')
-# print(results.result.rows)
-results = pd.DataFrame(data=results.result.rows)
-print('\n')
-print(results)
-print('\n')
-print(results.describe())
-print('\n')
-print(results.info())
+    with open(query_file, 'r', encoding='utf-8') as file:
+        query_text = file.read()
+
+    print(query_text + '\nlimit 20')
+
+    results = dune.run_sql(query_text + '\nlimit 20')
+    # print(results.result.rows)
+    results = pd.DataFrame(data=results.result.rows)
+    print('\n')
+    print(results)
+    print('\n')
+    print(results.describe())
+    print('\n')
+    print(results.info())
+else:
+    print('query id file not found, try again')
